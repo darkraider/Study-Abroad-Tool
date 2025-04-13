2cf5d15813428a58597b58abe5248a84adc2783b
darkraider <47706846+darkraider@users.noreply.github.com>
2025-04-13 08:05:11 -0500

    file changes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,24 @@
 import type { NextConfig } from "next";
 
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+
+  basePath: isProd ? '/Study-Abroad-Tool' : '',
+  assetPrefix: isProd ? '/Study-Abroad-Tool/' : '',
+
+  images: { 
+    unoptimized: true, 
+  }, 
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
+
 };
 
 export default nextConfig;
